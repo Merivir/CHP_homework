a903a8eabba35e5c81ae38d2e6f3daa333a14375
Merivir <118295002+Merivir@users.noreply.github.com>
2023-12-04 22:35:13 +0400

    Create TreeNode.hpp

---
+++ Hufmman/TreeNode.hpp
@@ -0,0 +1,20 @@
+#ifndef TREENODE_H
+#define TREENODE_H
+
+class TreeNode {
+public:
+    TreeNode(char symbol, int frequency);
+    char getSymbol() const;
+    int getFrequency() const;
+
+private:
+    char m_symbol;
+    int m_frequency;
+    TreeNode* m_left;
+    TreeNode* m_right;
+
+    // Responsible for constructing and destroying the Huffman tree
+    friend class HuffmanTree;
+};
+
+#endif
