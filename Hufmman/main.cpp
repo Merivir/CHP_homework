678a4549124ed723b9ff4706deaf4112de34a42d
Merivir <118295002+Merivir@users.noreply.github.com>
2023-12-04 22:39:31 +0400

    Create main.cpp

---
+++ Hufmman/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "HuffmanTree.hpp"
+
+int main() {
+    HuffmanTree myTree;
+    std::string text{"Hello world!"};
+    myTree.buildTree(text);
+    myTree.printTree();
+
+    return 0; // Indicates successful completion
+}
